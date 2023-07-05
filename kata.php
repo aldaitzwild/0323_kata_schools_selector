42d255a07ab30d240bf43bb132f76cd5a5915204
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-07-05 08:55:46 +0200

    Add a new kata

---
+++ kata.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Contact this URL => https://wizard-world-api.herokuapp.com/Houses
+ * (doc : https://wizard-world-api.herokuapp.com/swagger/index.html)
+ * and show each house you get with their name one line.
+ * To get all that, please use HttpClient (doc : https://symfony.com/doc/current/http_client.html)
+ * You already have it in your dependancies, so don't forget to do a
+ * composer install
+ */
+
+
+ // Don't touch that, you need it
+require_once('vendor/autoload.php');
